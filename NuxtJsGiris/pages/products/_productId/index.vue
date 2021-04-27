c6aed63fc7ee58a2b0783a414c2819172117bd35
Ali Osman <aosmanunal@gmail.com>
2021-04-27 23:46:01 +0300

    NuxtJsGiris

---
+++ NuxtJsGiris/pages/products/_productId/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h3>
+      Burası ürün özeti
+      <p>{{ $route.params.productId }}</p>
+
+      
+    </h3>
+  </div>
+</template>
+
+
+
+
+
+<style></style>
---
+++ NuxtJsGiris/pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h3>
+            Burası ürün sayfası
+        </h3>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
